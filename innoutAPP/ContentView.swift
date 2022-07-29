96c8b3de881194c412c31202dad10e74ea912d06
Eric Bregman <erbregman@gmail.com>
2022-07-29 12:58:08 -0700

    Initial Commit

---
+++ innoutAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  innoutAPP
+//
+//  Created by Eric Bregman on 7/29/22.
+//  Copyright © 2022 Sabertech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ innoutAPPTests/innoutAPPTests.swift
@@ -0,0 +1,34 @@
+//
+//  innoutAPPTests.swift
+//  innoutAPPTests
+//
+//  Created by Eric Bregman on 7/29/22.
+//  Copyright © 2022 Sabertech. All rights reserved.
+//
+
+import XCTest
+@testable import innoutAPP
+
+class innoutAPPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
