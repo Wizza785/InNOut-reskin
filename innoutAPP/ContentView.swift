8d021bca58eb361cbd47a0d4a8760d85a16ad0fe
Wizza785 <56855201+Wizza785@users.noreply.github.com>
2022-08-06 23:02:30 -0700

    Update ContentView.swift

--- innoutAPP/ContentView.swift
+++ innoutAPP/ContentView.swift
@@ -41,8 +41,8 @@ struct ContentView: View {
                     .foregroundColor(Color.secondary)
                 
                 Button(action: self.login) {
-                    Text("Sign In").foregroundColor(.white).padding().background(Rectangle().cornerRadius(10).foregroundColor(.red)).padding(20).offset(x: 0, y: -250)
-                }
+                    Text("Sign In")
+                }.foregroundColor(.white).padding().background(Rectangle().cornerRadius(10).foregroundColor(.red)).padding(20).offset(x: 0, y: -260)
             }
             .background(Group {
                 EmptyView()
@@ -65,11 +65,22 @@ struct ContentView: View {
         }.textFieldStyle(RoundedBorderTextFieldStyle())
    */
     }
-
+    func storeCookies(_ cookies: [HTTPCookie], forURL url: URL) {
+        let cookieStorage = HTTPCookieStorage.shared
+            cookieStorage.setCookies(cookies,
+                                     for: url,
+                                     mainDocumentURL: nil)
+        }
+        func readCookie(forURL url: URL) -> [HTTPCookie] {
+            let cookieStorage = HTTPCookieStorage.shared
+            let cookies = cookieStorage.cookies(for: url) ?? []
+            return cookies
+        }
     
     func login(){
-
         var semaphore = DispatchSemaphore (value: 0)
+       // let cookiesArray = HTTPCookieStorage.shared.cookies
+        //var cookiesStore =  URLSession.shared.configuration.httpCookieStorage?.cookies
 
         let parameters = "__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTc1NTk3ODQ4MA9kFgJmD2QWAgIDD2QWAgIHD2QWAgIBDxYCHglpbm5lcmh0bWwFBDIwMjJkZJPBmqku3rrzjmVB0CM%2FNXPW9oPC&__VIEWSTATEGENERATOR=CA0B0334&__EVENTVALIDATION=%2FwEdAAkxdEyNYrnWfP%2BB3o8xFTG8jJuJuoUx7HcZnXGSFYtde1%2FUteT%2F1FlG8zLcECMZNk0qlWIrJdTAOLR9iCT%2BsLLhweSyajcB5NIUzQwoZpYUMgIehXwdzgXcc6LchA%2FjzFQf1vHThVkYpfbc21rGBnU4w19%2FEt4D%2BIlXk1hba4TPaOOpLOOmr6thrBMsA7CKQHRvYmkeb6ahpZp0AtcjAr1KskgV6Q%3D%3D&ctl00%24Main%24ctrlLogin%24hdnViewer=1&ctl00%24Main%24ctrlLogin%24hdnVersion=0&ctl00%24Main%24ctrlLogin%24hfToken=&ctl00%24Main%24ctrlLogin%24hdnManifest=itms-services%3A%2F%2F%3Faction%3Ddownload-manifest%26url%3Dhttps%3A%2F%2Fassociate.innout.com%2Fapp%2FprodManifest.plist&ctl00%24Main%24ctrlLogin%24hdnUpdate=3.4.0&ctl00%24Main%24ctrlLogin%24txtAssociateID=\(userID)&ctl00%24Main%24ctrlLogin%24txtPIN=\(userPass)&ctl00%24Main%24ctrlLogin%24btnLogin=Login"
         let postData =  parameters.data(using: .utf8)
@@ -86,22 +97,27 @@ struct ContentView: View {
 
         request.httpMethod = "POST"
         request.httpBody = postData
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let session = URLSession.shared
+            session.configuration.httpCookieStorage = HTTPCookieStorage.shared
+            session.configuration.httpCookieAcceptPolicy = .always
+            session.configuration.httpShouldSetCookies = true
+        let task = session.dataTask(with: request) { data, response, error in
           guard let data = data else {
             print(String(describing: error))
             semaphore.signal()
             return
           }
-          print(String(data: data, encoding: .utf8)!)
+        // print(String(data: data, encoding: .utf8)!)
+        //print(cookiesArray ?? "nil")
           semaphore.signal()
         }
+        var cookies = readCookie(forURL:)
+        cookies = readCookie
+        print(cookies)
 
         task.resume()
         semaphore.wait()
-
-print($userID)
-        print($userPass)
+        
     }
     
 }
